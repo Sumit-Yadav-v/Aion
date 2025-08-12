7c35c2d92c7d9872e3bcacabfae3cd5808d01f03
Sumit-Yadav-v <sumit.yydv@gmail.com>
2025-08-12 18:18:09 +0530

    Update page.tsx
    changed server

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -25,11 +25,12 @@ const ChatUI = () => {
     setInput("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ text: userMessage }),
-      });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ text: userMessage }),
+});
+
 
       const data = await res.json();
 
